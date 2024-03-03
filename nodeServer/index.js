b672cdaed94ff1c627058b5078ce9ba1b785ddb8
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2024-03-03 15:36:15 +0530

    add comments

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,11 +1,15 @@
-// Node server which will handle socket io connections
+// Node server which will handle socket io connection
+
+// const { socket } = require('socket.io');
 const io = require('socket.io')(8000)
 
 const users = {};
 
 io.on('connection', socket =>{
+    // it is a socket.io instance server
     // If any new user joins, let other users connected to the server know!
     socket.on('new-user-joined', name =>{ 
+        // ek particular instance agar on hua to kya hona chaiye
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
